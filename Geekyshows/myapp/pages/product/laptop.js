7e7919434a06af7e434d250bdeab6dfd3683a682
satyam-seth <satyam1998.1998@gmail.com>
2023-11-26 00:50:46 +0530

    add pages links for all pages

--- Geekyshows/myapp/pages/product/laptop.js
+++ Geekyshows/myapp/pages/product/laptop.js
@@ -1,7 +1,16 @@
+import Link from 'next/link';
+
 export default function Laptop() {
     return (
         <div>
             <h1>Laptop Page</h1>
+            <ul>
+                <li><Link href="/">Home</Link></li>
+                <li><Link href="/about">About</Link></li>
+                <li><Link href="/product">Product</Link></li>
+                <li><Link href="/product/mobile">Mobile</Link></li>
+                <li><Link href="/product/laptop">Laptop</Link></li>
+            </ul>
         </div>
     );
 }
