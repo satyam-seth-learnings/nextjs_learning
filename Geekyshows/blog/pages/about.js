8224648daeb9d3fd7a77e403d92816f4f463f8ea
satyam-seth <satyam1998.1998@gmail.com>
2023-11-26 13:02:14 +0530

    add about page

---
+++ Geekyshows/blog/pages/about.js
@@ -0,0 +1,8 @@
+export default function About() {
+    return (
+        <>
+            <h1>About Page</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita, dolorum ut? Delectus, autem nulla iusto, quasi, dignissimos aspernatur dicta nesciunt eius saepe pariatur enim nostrum dolor consequatur tempore nemo quos?</p>
+        </>
+    )
+}
