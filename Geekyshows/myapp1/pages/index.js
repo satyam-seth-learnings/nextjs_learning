d7584f85e76d19845eeeaa7c88f83c47d99e063b
satyam-seth <satyam1998.1998@gmail.com>
2023-11-26 12:26:55 +0530

    add get static props example

---
+++ Geekyshows/myapp1/pages/index.js
@@ -0,0 +1,23 @@
+export default function Home({data}) {
+  console.log(data);
+
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <h2>{data.title}</h2>
+      <p>{data.body}</p>
+    </div>
+  )
+}
+
+export async function getStaticProps() {
+  // Data Fetching
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts/5");
+  const data = await res.json();
+
+  return {
+    props: {
+      data
+    }
+  };
+}
