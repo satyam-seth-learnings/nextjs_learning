e588c5385989ed9f49d52babf35cc0591f19de03
satyam-seth <satyam1998.1998@gmail.com>
2023-12-31 13:27:16 +0530

    Add Server Component and Client Component Example

--- Geekyshows/Master Next JS 14 Complete Basic to Advance/ch3/src/app/page.jsx
+++ Geekyshows/Master Next JS 14 Complete Basic to Advance/ch3/src/app/page.jsx
@@ -1,12 +1,15 @@
-import LearnLink from '@/components/LearnLink'
-import LearnUseRouter from '@/components/LearnUseRouter'
-import Image from 'next/image'
+import LearnClientComponent from '@/components/LearnClientComponent';
+import LearnServerComponent from '@/components/LearnServerComponent';
+import LearnLink from '@/components/LearnLink';
+import LearnUseRouter from '@/components/LearnUseRouter';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       {/* <LearnLink /> */}
-      <LearnUseRouter />
+      {/* <LearnUseRouter /> */}
+      {/* <LearnServerComponent /> */}
+      <LearnClientComponent />
     </main>
   )
 }
