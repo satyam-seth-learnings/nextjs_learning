11afd40ad22d6531ce3dff1b6a175ca1014ce120
satyam-seth <satyam1998.1998@gmail.com>
2024-01-01 02:02:32 +0530

    Server Action

---
+++ Geekyshows/Master Next JS 14 Complete Basic to Advance/ch5/src/app/components/GetData2.jsx
@@ -0,0 +1,23 @@
+import connectDB from "@/lib/connectDB";
+import PostModel from "@/models/Post";
+
+export default async function GetData2() {
+    await connectDB();
+
+    const allPosts = await PostModel.find();
+  
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        {
+          allPosts.map((post, i) => (
+            <div key={i}>
+              <h1 className="text-red-500">{post.title}</h1>
+              <p>{post.title}</p>
+              <hr />
+            </div>
+          ))
+        }
+      </main>
+    )
+  }
+  
--- Geekyshows/Master Next JS 14 Complete Basic to Advance/ch5/src/app/page.js
+++ Geekyshows/Master Next JS 14 Complete Basic to Advance/ch5/src/app/page.js
@@ -1,11 +1,15 @@
 import GetData1 from "./components/GetData1";
 import PostData1 from "./components/PostData1";
+import GetData2 from "./components/GetData2";
+import PostData2 from "./components/PostData2";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      {/* <PostData1 /> */}
       {/* <GetData1 /> */}
-      <PostData1 />
+      {/* <GetData2 /> */}
+      <PostData2 />
     </main>
   )
 }
