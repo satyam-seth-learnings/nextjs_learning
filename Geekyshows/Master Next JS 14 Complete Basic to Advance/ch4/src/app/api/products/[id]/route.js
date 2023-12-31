df740e2fe82eb1b72be67e045208195e5a5aeab4
satyam-seth <satyam1998.1998@gmail.com>
2023-12-31 21:43:06 +0530

    Params

--- Geekyshows/Master Next JS 14 Complete Basic to Advance/ch4/src/app/api/products/[id]/route.js
+++ Geekyshows/Master Next JS 14 Complete Basic to Advance/ch4/src/app/api/products/[id]/route.js
@@ -1,7 +1,16 @@
 import { NextResponse } from "next/server";
 
-export const GET = async (req) => {
+export const GET = async (req, context) => {
     console.log("Req: ", req);
+    console.log("Context: ", context);
+    console.log("Context.params: ", context.params);
+    console.log("Context.params.id: ", context.params.id);
 
     return NextResponse.json({ "msg": "success" }, { status: 200 })
 };
+
+// export const GET = async (req, { params }) => {
+//     console.log("Params.id: ", params.id);
+
+//     return NextResponse.json({ "msg": "success" }, { status: 200 })
+// };
