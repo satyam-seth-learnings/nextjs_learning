e4f34da97735aa58d1e3665678f443d2052f9700
satyam-seth <satyam1998.1998@gmail.com>
2023-12-31 19:29:45 +0530

    Request Headers

--- Geekyshows/Master Next JS 14 Complete Basic to Advance/ch4/src/app/api/products/route.js
+++ Geekyshows/Master Next JS 14 Complete Basic to Advance/ch4/src/app/api/products/route.js
@@ -2,5 +2,10 @@ import { NextResponse } from "next/server";
 
 export async function GET(req) {
     console.log(req);
+
+    // Headers
+    const requestHeaders = new Headers(req.headers);
+    console.log(requestHeaders);
+
     return NextResponse.json("Hello NextJS API");
 }
