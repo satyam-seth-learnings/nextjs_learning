a3bf7216c1322bcccaa39985cced2dfd6e333b94
satyam-seth <satyam1998.1998@gmail.com>
2023-12-31 19:33:24 +0530

    URL Query Params

--- Geekyshows/Master Next JS 14 Complete Basic to Advance/ch4/src/app/api/products/route.js
+++ Geekyshows/Master Next JS 14 Complete Basic to Advance/ch4/src/app/api/products/route.js
@@ -7,5 +7,13 @@ export async function GET(req) {
     const requestHeaders = new Headers(req.headers);
     console.log(requestHeaders);
 
+    // URL Query Params
+    // const { searchParams } = new URL(req.url);
+    const searchParams = req.nextUrl.searchParams;
+    console.log(searchParams);
+    console.log(searchParams.get("search"));
+
+
+
     return NextResponse.json("Hello NextJS API");
 }
