77ebc296b5c875dff085d26e00813b8241ddfa24
satyam-seth <satyam1998.1998@gmail.com>
2023-12-31 13:08:14 +0530

    Add Link Example

--- Geekyshows/Master Next JS 14 Complete Basic to Advance/ch3/src/app/page.jsx
+++ Geekyshows/Master Next JS 14 Complete Basic to Advance/ch3/src/app/page.jsx
@@ -1,9 +1,10 @@
+import LearnLink from '@/components/LearnLink'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello Next JS</h1>
+      <LearnLink />
     </main>
   )
 }
---
+++ Geekyshows/Master Next JS 14 Complete Basic to Advance/ch3/src/components/LearnLink.jsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+const LearnLink = () => {
+    const id = 2;
+
+    return (
+        <>
+            <Link href="/admin/dashboard/">Go to Admin Dashboard</Link>
+            <Link href={`/user/profile/${id}`}>Go to User Profile</Link>
+        </>
+    );
+}
+
+export default LearnLink;
