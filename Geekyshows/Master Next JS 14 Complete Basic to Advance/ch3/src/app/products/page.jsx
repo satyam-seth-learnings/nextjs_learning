bbee8b1d6988ec619d3a1bf2cf8cb5b7deb6c0c6
satyam-seth <satyam1998.1998@gmail.com>
2023-12-31 11:53:58 +0530

    Add Route Example

---
+++ Geekyshows/Master Next JS 14 Complete Basic to Advance/ch3/src/app/products/page.jsx
@@ -0,0 +1,11 @@
+const Products = () => {
+    return (
+        <>
+            <h1>Product Page</h1>
+            <hr />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus ad maiores facilis repudiandae vel in adipisci numquam. Explicabo enim ullam ex voluptatem repudiandae magnam eum pariatur, nobis beatae veritatis obcaecati?</p>
+        </>
+    );
+};
+
+export default Products;
