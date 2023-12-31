47e00c2db3dda9e94aa98324963d7bcab43973df
satyam-seth <satyam1998.1998@gmail.com>
2024-01-01 01:04:36 +0530

    Show Retrieved Data in Web Page

--- Geekyshows/Master Next JS 14 Complete Basic to Advance/ch5/src/app/page.js
+++ Geekyshows/Master Next JS 14 Complete Basic to Advance/ch5/src/app/page.js
@@ -1,7 +1,9 @@
+import GetData1 from "./components/GetData1";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Next JS</h1>
+      <GetData1 />
     </main>
   )
 }
