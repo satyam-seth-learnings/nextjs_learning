34803b04aebd0ac81b13854a067091181c999de6
satyam-seth <satyam1998.1998@gmail.com>
2024-01-01 02:05:09 +0530

    Change Title - Metadata

--- Geekyshows/Master Next JS 14 Complete Basic to Advance/ch5/src/app/page.js
+++ Geekyshows/Master Next JS 14 Complete Basic to Advance/ch5/src/app/page.js
@@ -3,13 +3,18 @@ import PostData1 from "./components/PostData1";
 import GetData2 from "./components/GetData2";
 import PostData2 from "./components/PostData2";
 
+export const metadata = {
+  title: "Blog Posts",
+  description: "This is blog post all"
+}
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       {/* <PostData1 /> */}
       {/* <GetData1 /> */}
-      {/* <GetData2 /> */}
-      <PostData2 />
+      <GetData2 />
+      {/* <PostData2 /> */}
     </main>
   )
 }
